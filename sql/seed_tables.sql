d9214884571301900f7ab211c8112e188597dd33
Vei Yie <veiyie1998@gmail.com>
2021-09-03 07:41:10 +0800

    Updates images

--- sql/seed_tables.sql
+++ sql/seed_tables.sql
@@ -2,5 +2,5 @@ INSERT INTO users (name, post)
 VALUES
  ('Jamie Brown', 'Using this new post app, it''s not very fleshed out'),
  ('Bobby Brown', 'Books make me happy'),
- ('Kylie Jenner', 'I love Kanye'),
- ('Kendal Jenner', 'I love flirting');
+ ('Vei Yie', 'I love drinking'),
+ ('Mary', 'Little rogue matcha');
