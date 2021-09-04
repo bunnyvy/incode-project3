daa1a09afe341029fb05b561a6e3fc3cd3129f57
Vei Yie <veiyie1998@gmail.com>
2021-09-04 16:48:54 +0800

    adds tables attributes

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    start_at TIME NOT NULL,
+    end_at TIME NOT NULL
 );
 
